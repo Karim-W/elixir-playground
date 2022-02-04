aefa17904cbe4a0e103c50bba5c770e732b12735
Karim <karim.wael@gmail.com>
2022-02-04 23:58:26 +0400

    Array plus loops

--- elixirplayground/dataStructures.exs
+++ elixirplayground/dataStructures.exs
@@ -2,5 +2,13 @@
 
 # MARK: LISTS
 
+defmodule Arrayss do
+  arr = ["Luke", "Han", "Leia", "Chewbacca", "R2D2"]
+  ## Loop Through an Array with a For Loop
+  for item <- arr do
+    IO.inspect(item)
+  end
+end
+
 defmodule Listss do
 end
